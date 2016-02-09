b801e56d1e2b09d3f4456e03e4a36a69d555e6bb
Charlie Hebert <charlie.hebert92@gmail.com>
2016-02-09 19:12:27 +0000

    Remove skeleton authentication module

--- distributed-file-server/server_client_package/authentication/authentication.py
+++
@@ -1,6 +0,0 @@
-from server.server_core.request_handler import ThreadedTCPRequestHandler
-
-
-class AuthenticationHandler(ThreadedTCPRequestHandler):
-    def handle_message(self):
-        print "I'm an authentication handler!"
